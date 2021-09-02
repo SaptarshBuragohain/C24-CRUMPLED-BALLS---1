0a9fb879d634ca74671b0c5fd84e9925cc30385e
SaptarshBuragohain <87750199+SaptarshBuragohain@users.noreply.github.com>
2021-09-02 07:23:45 +0400

    Add files via upload

--- paper.js
+++ paper.js
@@ -1,30 +1,20 @@
 class Paper {
-  constructor(x, y, radius) {  
-     var options = {
-        'restitution':0.3,
-        'friction':0.7,
-        'density':1.2,
+  constructor(x, y, radius) {
+    var options = {
+        isStatic:false,
+        restitution:0.3,
+        friction:0.5,
+        density:1.2
     }
     this.body = Bodies.circle(x, y, radius, options);
-    this.image = loadImage("paper.png");
-    this.width = 60
-    this.height= 60
-    
-      
-    
+    this.radius = radius
     World.add(world, this.body);
   }
   display(){
-    var pos =this.body.position;
-    var angle = this.body.angle;
-    push();
-    translate(pos.x, pos.y);
-    rotate(angle);
     ellipseMode(RADIUS);
     strokeWeight(4);
-    stroke("white")
+    stroke("blue")
     fill(255);
-    image(this.image, 0, 0, this.width, this.height);
-    pop();
+    circle(this.body.position.x,this.body.position.y,this.radius)
   }
 };
